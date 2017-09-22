c633d51df925d323fede41e16636dbd03168da92
Ulrich <backes@3u.net>
2017-09-22 16:26:26 +0200

    First commit

---
+++ MyFamousLibrary/Person.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace MyFamousLibrary
+{
+    /// <summary>
+    /// This is my class 'Person'
+    /// </summary>
+    public class Person
+    {
+        private string name;
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        /// <exception cref="ArgumentNullException">Name</exception>
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value == null) throw new ArgumentNullException(nameof(Name));
+                name = value;
+            }
+        }
+
+        private int age;
+
+
+        /// <summary>
+        /// Gets or sets the age.
+        /// </summary>
+        /// <value>
+        /// The age.
+        /// </value>
+        /// <exception cref="ArgumentOutOfRangeException">Age</exception>
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (age < 0 || age > 150) throw new ArgumentOutOfRangeException(nameof(Age));
+
+                age = value;
+
+            }
+        }
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Person"/> class.
+        /// </summary>
+        /// Text before Person
+        public Person()
+        {
+            Name = "Peter";
+        }
+
+        /// <summary>
+        /// Increments the age.
+        /// </summary>
+        /// <param name="years">Used to define the incrementation step</param>
+        /// <paramref name="years">Used to define the incrementation step</paramref>
+        public void IncrementAge(int years)
+        {
+            Age += years;
+            return;
+        }
+
+        /// <summary>
+        /// Description of myMethod
+        /// </summary>
+        /// <param name="a">Description of a ...</param>
+        /// <param name="b">Description of b ...</param>
+        private void myMethodWithTwoParams(int a, int b)
+        {
+            return;
+        }
+
+
+        /// <summary>
+        /// Description of this method ...
+        /// </summary>
+        /// <param name="name">Description of name ...</param>
+        /// <returns>Returns an empty string</returns>
+        public string mySecondMethodWithReturnvalue(string name)
+        {
+            return "";
+        }
+
+        /// <summary>
+        /// Description of this method ...
+        /// </summary>
+        /// <remarks>Some additional informations ...<see cref="Age"/></remarks>
+        /// 
+        public void myThirdMethodWithRemark()
+        {
+
+        }
+
+        /// <summary>
+        /// Description of this method ...
+        /// </summary>
+        /// <typeparam name="T">Description of T ...</typeparam>
+        public void myGenericMethod<T>()
+        {
+
+        }
+
+        /// <summary>
+        /// Mies the private method.
+        /// </summary>
+        /// <param name="a">a.</param>
+        /// <param name="b">The b.</param>
+        /// <returns>The sum of a and b</returns>
+        private string myPrivateMethod(int a, int b)
+        {
+            return (a + b).ToString();
+        }
+
+        /// <summary>
+        /// Adds the person.
+        /// </summary>
+        /// <param name="a">a. Further descr....</param>
+        /// <returns>Returns an empty string</returns>
+        public string AddPerson(int a)
+        {
+            return "";
+        }
+
+        
+
+    }
+}
---
+++ MyFamousLibrary_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyFamousLibrary_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyFamousLibrary_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a75500b3-9c86-4dfe-966a-b0f4104b0f91")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
